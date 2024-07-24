cee45d224a4e166be69b27b78b57deb7f27398ad
laeba <laeba2704@gmail.com>
2024-07-25 00:35:38 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@
 import Image from "next/image";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
-
+import Head from 'next/head';
 export default function Home() {
   return (
    <div>
+    <Head>
+     <meta name="google-site-verification" content="TdMZcCgup5n4hjeG2MIGCa8DzJj41atsbr3-gy55xIc" />
+      </Head>
   <Header/>
   <Hero/>
    </div>
